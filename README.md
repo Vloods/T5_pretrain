d4d34061dc48bc0a6f264822cf84530412fafe49
Vloods <lmvibe@outlook.com>
2024-11-16 12:12:11 +0300

    chore: refactor for coling submit

--- README.md
+++ README.md
@@ -10,7 +10,7 @@ This repo provides the **source code & data** of our paper "Multitask Finetuning
 
 
 ## 0. Dependencies
-Installation guide for training **Roberta/T5** with H/A 100  
+Installation guide for training **Roberta** with H/A 100  
 
 **Requirements:** python3.8, cuda11.8, torch2.0.1, pyg2.4, transformers4.10
 
@@ -32,7 +32,6 @@ Download pretrained models and place files under `./models`
 
 | Model  | Size | Pretraining Text | Pretraining Knowledge Graph | Download Link |
 | ------------- | --------- | ---- | ---- | ---- |
-| T5-base | 220M parameters | BookCorpus (filtered) | ConceptNet | link (will be updated) |
 | RoBERTa | 360M parameters | BookCorpus (filtered) | ConceptNet | link (will be updated) |
 
 
@@ -55,7 +54,6 @@ scripts/run_train__joint.sh
 **(Optional)** To pretrain model on your own data, you can run:
 ```
 scripts/run_pretrain.sh
-scripts/T5_run_pretrain.sh
 ```
 
 ## Acknowledgment
--- scripts/run_train__joint.sh
+++ scripts/run_train__joint.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-export CUDA_VISIBLE_DEVICES=0,1
+export CUDA_VISIBLE_DEVICES=2,3
 export TOKENIZERS_PARALLELISM=true
-export OMP_NUM_THREADS=4
+export OMP_NUM_THREADS=6
 dt=`date '+%Y%m%d_%H%M%S'`
 
 
-dataset="obqa"
+dataset="dream"
 dataset_mrc="csqa"
+dataset_kgqa='kqa_pro_resplit'
 shift
 encoder='roberta-large'
 args=$@
@@ -16,7 +17,12 @@ args=$@
 elr="3e-5"
 dlr="1e-3"
 bs=128
+bsm=64
+bsk=128
 mbs=2
+mbsm=8
+mbsk=16
+
 unfreeze_epoch=2
 k=5 #num of gnn layers
 residual_ie=2
@@ -29,17 +35,18 @@ seed=1
 lr_schedule=warmup_linear
 warmup_steps=100
 
-n_epochs=40
+n_epochs=22
 max_epochs_before_stop=30
 ie_dim=400
 
 
-max_seq_len=100
+max_seq_len=128
 ent_emb=data/cpnet/tzw.ent.npy
 kg=cpnet
 kg_vocab_path=data/cpnet/concept.txt
-inhouse=False
+inhouse=false
 
+ent_emb_kgqa=data/kqa_pro_graph/embeddings_kqa_pro_unmapped_fixed_32.npy
 
 info_exchange=true
 ie_layer_num=1
@@ -66,18 +73,18 @@ save_dir_pref='runs'
 mkdir -p $save_dir_pref
 mkdir -p logs
 
-run_name=dragon__${dataset}_${dataset_mrc}_ih_${inhouse}_load__elr${elr}_dlr${dlr}_W${warmup_steps}_b${bs}_ufz${unfreeze_epoch}_e${n_epochs}_sd${seed}__${dt}
+run_name=dragon__${dataset}_${dataset_mrc}_${dataset_kgqa}_ih_${inhouse}_load__elr${elr}_dlr${dlr}_W${warmup_steps}_b${bs}_ufz${unfreeze_epoch}_e${n_epochs}_sd${seed}__${dt}
 log=logs/train__${run_name}.log.txt
 
 ###### Training ######
-python3 -u dragon_joint.py \
-    --dataset $dataset --dataset_mrc $dataset_mrc\
-    --encoder $encoder -k $k --gnn_dim $gnndim -elr $elr -dlr $dlr -bs $bs --seed $seed -mbs ${mbs} --unfreeze_epoch ${unfreeze_epoch} --encoder_layer=${encoder_layer} -sl ${max_seq_len} --max_node_num ${max_node_num} \
-    --n_epochs $n_epochs --max_epochs_before_stop ${max_epochs_before_stop} --fp16 $fp16 --upcast $upcast --use_wandb false \
+python -u dragon_joint.py \
+    --dataset $dataset --dataset_mrc $dataset_mrc --dataset_kgqa $dataset_kgqa\
+    --encoder $encoder -k $k --gnn_dim $gnndim -elr $elr -dlr $dlr -bs $bs -bsm $bsm -bsk $bsk --seed $seed -mbs ${mbs} -mbsm ${mbsm} -mbsk ${mbsk} --unfreeze_epoch ${unfreeze_epoch} --encoder_layer=${encoder_layer} -sl ${max_seq_len} --max_node_num ${max_node_num} \
+    --n_epochs $n_epochs --max_epochs_before_stop ${max_epochs_before_stop} --fp16 $fp16 --upcast $upcast --use_wandb true \
     --save_dir ${save_dir_pref}/${dataset}/${run_name} --save_model 1 \
     --run_name ${run_name} \
     --load_model_path $load_model_path \
     --residual_ie $residual_ie \
-    --ie_dim ${ie_dim} --info_exchange ${info_exchange} --ie_layer_num ${ie_layer_num} --resume_checkpoint ${resume_checkpoint} --resume_id ${resume_id} --sep_ie_layers ${sep_ie_layers} --random_ent_emb ${random_ent_emb} --ent_emb_paths ${ent_emb//,/ } --lr_schedule ${lr_schedule} --warmup_steps $warmup_steps -ih ${inhouse} --kg $kg --kg_vocab_path $kg_vocab_path \
+    --ie_dim ${ie_dim} --info_exchange ${info_exchange} --ie_layer_num ${ie_layer_num} --resume_checkpoint ${resume_checkpoint} --resume_id ${resume_id} --sep_ie_layers ${sep_ie_layers} --random_ent_emb ${random_ent_emb} --ent_emb_paths ${ent_emb//,/ } --lr_schedule ${lr_schedule} --warmup_steps $warmup_steps -ih ${inhouse} --kg $kg --kg_vocab_path $kg_vocab_path --ent_emb_paths_kgqa ${ent_emb_kgqa//,/ } \
     --data_dir data \
 > ${log}
